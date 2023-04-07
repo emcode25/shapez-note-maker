2c677c920c37aa353517c29682a0195d4c028c97
Evan <enhcode@gmail.com>
2023-04-07 17:21:01 -0400

    Started from base.js in the Shapez.io documentation.

---
+++ NoteMaker.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+const METADATA = {
+    website: "https://github.com/emcode25",
+    author: "Evan Molinares",
+    name: "NoteMaker",
+    version: "0.1",
+    id: "notemaker",
+    description: "Allows the placement of onscreen notes.",
+    minimumGameVersion: ">=1.5.0",
+	
+    doesNotAffectSavegame: true,
+};
+
+class Mod extends shapez.Mod {
+    init() {
+        // Start the modding here
+    }
+}
